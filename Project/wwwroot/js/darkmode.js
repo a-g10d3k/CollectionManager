275a588e84769eefc83ce0b7a1ffd38a17bdf00f
Ariadna Głodek <a.glodek@gmail.com>
2022-10-24 18:57:17 +0200

    Added darkmode and a toggle on the navbar

---
+++ Project/wwwroot/js/darkmode.js
@@ -0,0 +1,7 @@
+﻿$('input[name="darkmode-toggle"]').change(function () {
+    if ($('input[name="darkmode-toggle"]:checked').val() == 'true') {
+        $('body').addClass('dark');
+    } else {
+        $('body').removeClass('dark');
+    }
+});
