73d10d16cf5dbd4aa7fbecd243bee0265d2fe074
Ariadna Głodek <a.glodek@gmail.com>
2022-10-26 14:51:38 +0200

    User view now only loads 8 collections per page

--- Project/Controllers/UsersController.cs
+++ Project/Controllers/UsersController.cs
@@ -11,6 +11,8 @@ namespace Project.Controllers
         private AppDbContext _context;
         private UserManager<ApplicationUser> _userManager;
 
+        public const int CollectionsPerPage = 8;
+
         public UsersController(AppDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
@@ -19,15 +21,23 @@ namespace Project.Controllers
 
         [HttpGet]
         [Route("{controller}/{username}", Name = "GetUser")]
-        public async Task<IActionResult> GetUser(string username)
+        public async Task<IActionResult> GetUser(string username, [FromQuery] int page = 1)
         {
-            var query = _context.Users.Where(u => u.UserName == username)
-                .Include(u => u.Collections);
+            if (page < 1) page = 1;
+            var query = _context.Users.Where(u => u.UserName == username).Select(u => new UserDto
+            {
+                User = u,
+                Collections = u.Collections.OrderByDescending(c => c.Created)
+                .Skip((page - 1) * CollectionsPerPage)
+                .Take(CollectionsPerPage).ToList(),
+                maxPage = u.Collections.Count() / (CollectionsPerPage + 1) + 1
+            });
             if (!query.Any()) return NotFound();
             var user = await query.FirstAsync();
             var currentUser = await _userManager.GetUserAsync(User);
-            bool isOwner = await _userManager.IsInRoleAsync(user, "Admin") || user == currentUser;
-            ViewData["IsOwner"] = isOwner;
+            bool isOwner = await _userManager.IsInRoleAsync(user.User, "Admin") || user.User == currentUser;
+            user.IsOwner = isOwner;
+            user.page = page;
             return View(user);
         }
     }
---
+++ Project/Models/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Models
+{
+    public class UserDto
+    {
+        public ApplicationUser User { get; set; }
+        public List<Collection> Collections { get; set; }
+        public bool IsOwner { get; set; } = false;
+        public int page { get; set; } = 1;
+        public int maxPage { get; set; } = 1;
+    }
+}
